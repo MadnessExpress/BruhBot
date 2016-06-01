55efb5e2b99c089d2c0755ac23580addfc0df2c3
FormalHellhound <FormalHellhound@gmail.com>
2016-06-01 16:04:18 -0400

    Moved 8ball config to 8ball-config.yml

--- config.rb
+++ config.rb
@@ -33,19 +33,6 @@ def choicemessage()
 
 end
 
-#Define 8ball outcomes below
-
-def eightball()
-
-  eightball = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes, definitely.', 'You may rely on it.',
-           'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes', 'Signs point to yes', 'Reply hazy try again',
-           'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again',
-           'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful'] 
-
-  return eightball
-
-end
-
 #Define reaction images below
 
 def smug()
--- plugins/commands/8ball.rb
+++ plugins/commands/8ball.rb
@@ -1,10 +1,14 @@
 module Eightball
 
+  data = YAML::load_file(File.join(__dir__, 'config/8ball-config.yml'))
+
   extend Discordrb::Commands::CommandContainer
 
   command(%s(8ball), min_args: 1, description: 'Consult the magic 8ball', usage: '8ball <question>') do |event|
 
-    event.respond ":8ball: #{eightball.sample}"
+    eightball = data["eightball"].sample
+
+    event.respond ":8ball: #{eightball}"
 
   end
 
