ed5594d3661d6772f0c93f0d236cd9d5e99d9b67
FormalHellhound <FormalHellhound@gmail.com>
2016-06-30 14:27:43 -0400

    Added pastebin plugin.

---
+++ plugins/commands/pastebin.rb
@@ -0,0 +1,15 @@
+module Paste
+
+  extend Discordrb::Commands::CommandContainer
+
+  command(:paste, min_args: 1, description: "Creates a Pastebin paste with the specified text.", usage: "paste <text>") do |event, *text|
+
+    apidata = YAML::load_file(File.join(__dir__, '../../apikeys.yml'))
+
+    pastebin = Pastebin::Client.new(apidata["pastebinapikey"])
+
+    event.respond pastebin.newpaste(text.join(" "))
+
+  end
+
+end
